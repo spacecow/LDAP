0d6495ea986f360ca15a3346a10e87dcbe7e3180
Johan Sveholm <jsveholm@gmail.com>
2011-11-24 14:21:06 +0900

    first commit for deploy

---
+++ Gemfile
@@ -0,0 +1,20 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.10'
+gem 'mysql2', '0.2.6'
+
+group :development do
+  gem "rspec-rails"
+end
+
+group :test do
+  gem 'libnotify'
+  gem 'spork', '> 0.9.0.rc'
+  gem 'capybara' #, :git => 'https://github.com/jnicklas/capybara.git'
+  gem 'database_cleaner'
+  gem 'launchy'
+  gem 'factory_girl_rails'
+  gem 'guard'
+  gem 'guard-rspec'
+  gem 'guard-spork'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LDAP::Application.config.secret_token = 'd8f3fad903bc55b5f16aaf0c327f87ca28adc4fa9e4b5aa21fbbfd33c9a5efd25708a5e6120227875001e3db77ca82dec59830acab167a738dec8e03a36771ae'
