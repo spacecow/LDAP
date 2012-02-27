2b42cf19465ab6876986de2b6cdebd5e875fa444
Johan Sveholm <jsveholm@gmail.com>
2012-02-27 15:34:59 +0900

    link to output to xls

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,8 +6,4 @@ module ApplicationHelper
     direction = col == sort_column && sort_direction == 'asc' ? 'desc' : 'asc'
     link_to title, {:sort => col, :direction => direction}, {:class => css_class}
   end
-
-  def csv_button_to
-    button_to 'CSV', report_path(1, :sort => params[:sort], :direction => params[:direction], :format => :xls)
-  end
 end
